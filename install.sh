875234f14bb7f8277a9b02df8511b9546349c36b
USawesome <96547758+USawesome@users.noreply.github.com>
2024-07-30 16:49:40 -0400

    Cleanup java removal

--- install.sh
+++ install.sh
@@ -7,3 +7,4 @@ mkdir java/
 cd java/
 wget https://cdn.azul.com/zing-zvm/ZVM24.06.0.0/zing24.06.0.0-2-jdk21.0.3-linux_x64.tar.gz
 tar -xf zing24.06.0.0-2-jdk21.0.3-linux_x64.tar.gz
+rm zing24.06.0.0-2-jdk21.0.3-linux_x64.tar.gz
