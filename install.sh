3ba1d83117a314fb3d8ea8627ae4fd2e8d064591
USawesome <96547758+USawesome@users.noreply.github.com>
2024-07-30 14:22:42 -0400

    Add server.jar installation

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+wget https://piston-data.mojang.com/v1/objects/450698d1863ab5180c25d7c804ef0fe6369dd1ba/server.jar
 mkdir java/
 cd java/
 wget https://cdn.azul.com/zing-zvm/ZVM24.06.0.0/zing24.06.0.0-2-jdk21.0.3-linux_x64.tar.gz
